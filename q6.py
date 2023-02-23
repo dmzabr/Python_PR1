811dad94250b8c149d2e40db9bd6fa37703dc27e
Дмитрий Забродский <dmitrijzabrodskij@MacBook-Air-Dmitrij.local>
2023-02-23 15:31:57 +0300

    Удалены временные переменные из программы

--- q6.py
+++ q6.py
@@ -1,7 +1,4 @@
 print("Введите номер билета")
 billet = int(input())
-a = billet/100000
-b = (billet/10000)%10
-c = (billet/1000)%10
 print("Yes" if int(billet/100000) + int((billet/10000)%10) + int((billet/1000)%10) == 
       int(billet%10) + int((billet%100)/10) + int((billet%1000)/100) else "No")
